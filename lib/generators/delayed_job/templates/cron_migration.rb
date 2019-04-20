77f99beb0454dee0f697f320ad5e8946d0cd544a
localhost.dev <localhostdotdev@protonmail.com>
2019-04-20 15:00:31 +0200

    moder migration

--- lib/generators/delayed_job/templates/cron_migration.rb
+++ lib/generators/delayed_job/templates/cron_migration.rb
@@ -1,9 +1,5 @@
 class AddCronToDelayedJobs < ActiveRecord::Migration<%= migration_version %>
-  def self.up
+  def change
     add_column :delayed_jobs, :cron, :string
-  end
-
-  def self.down
-    remove_column :delayed_jobs, :cron
   end
 end
