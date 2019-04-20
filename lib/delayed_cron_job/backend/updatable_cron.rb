0c81cefba54c70a71a59afc482c15911db416f65
localhost.dev <localhostdotdev@protonmail.com>
2019-04-20 15:09:20 +0200

    more formatting

--- lib/delayed_cron_job/backend/updatable_cron.rb
+++ lib/delayed_cron_job/backend/updatable_cron.rb
@@ -1,9 +1,8 @@
 module DelayedCronJob
   module Backend
     module UpdatableCron
-
       def self.included(klass)
-        klass.send(:before_save, :set_next_run_at, :if => :cron_changed?)
+        klass.send(:before_save, :set_next_run_at, if: :cron_changed?)
       end
 
       def set_next_run_at
@@ -11,7 +10,6 @@ module DelayedCronJob
           self.run_at = Cronline.new(cron).next_time(Delayed::Job.db_time_now)
         end
       end
-
     end
   end
 end
