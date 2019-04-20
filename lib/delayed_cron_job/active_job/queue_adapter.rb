933e0f0dc755b03bf7af7af8db974c3123199e06
localhost.dev <localhostdotdev@protonmail.com>
2019-04-20 15:08:19 +0200

    more formatting

--- lib/delayed_cron_job/active_job/queue_adapter.rb
+++ lib/delayed_cron_job/active_job/queue_adapter.rb
@@ -1,7 +1,6 @@
 module DelayedCronJob
   module ActiveJob
     module QueueAdapter
-
       def self.included(klass)
         klass.send(:alias_method, :enqueue, :enqueue_with_cron)
         klass.send(:alias_method, :enqueue_at, :enqueue_at_with_cron)
@@ -18,16 +17,19 @@ module DelayedCronJob
       end
 
       def enqueue_at_with_cron(job, timestamp)
-        options = { queue: job.queue_name,
-                    cron: job.cron  }
+        options = {
+          queue: job.queue_name,
+          cron: job.cron
+        }
+
         options[:run_at] = Time.at(timestamp) if timestamp
         options[:priority] = job.priority if job.respond_to?(:priority)
+
         wrapper = ::ActiveJob::QueueAdapters::DelayedJobAdapter::JobWrapper.new(job.serialize)
         delayed_job = Delayed::Job.enqueue(wrapper, options)
         job.provider_job_id = delayed_job.id if job.respond_to?(:provider_job_id=)
         delayed_job
       end
-
     end
   end
 end
